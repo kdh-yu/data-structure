3a1deafeb71c930d2eb7a47e48a4f64832be7789
tt-adisoh <ktx030430@naver.com>
2023-03-30 11:53:50 +0900

    comment

--- Stack, Queue/practice4.cpp
+++ Stack, Queue/practice4.cpp
@@ -7,14 +7,22 @@ using namespace std;
 const char PALINDROME = 'P';
 const char BALANCE = 'B';
 
-// Return true if str is a palindrome; false otherwise
+/*
+Return true if str is a palindrome; false otherwise
+Time complexity is O(n), since we need to check
+the whole length n string.
+Popping componenets takes O(1).
+*/
 bool isPalindrome(string& str) {
+  // add each char to deque
   deque<char> deq;
   for (int i=0; i<str.length(); i++)
     deq.push_back(str[i]);
   
+  // Bi-directionally checking
   while (deq.size() >= 2) {
-    if (deq.front() != deq.back()) return false;
+    if (deq.front() != deq.back())
+      return false;
     deq.pop_front();
     deq.pop_back();
   }
@@ -23,25 +31,37 @@ bool isPalindrome(string& str) {
 }
 
 // Return true if brackets are balanced in str; false otherwise
+// Time complexity : O(n)
+// Because we need to check whole length n string
 bool balance(string& str) {
   deque<char> deq;
   map<char, char> brackets;
   string bracket = "{[()]}";
+  // I used dictionary in Python, but we can't use dictionary
+  // Instead, there is map
+  // it's like hash, so it takes O(1)
   brackets['}'] = '{';
   brackets[']'] = '[';
   brackets[')'] = '(';
 
   for (int i=0; i<str.length(); i++) {
+    // pass if it is not bracket
     if (bracket.find(str[i]) == string::npos);
+    // stack is empty or opening bracket, then append it
+    // bracket.substr(0,3) == "{[("
     else if (deq.empty() || bracket.substr(0,3).find(str[i]) != string::npos)
       deq.push_back(str[i]);
+    // If matched well, instead of appending it, pop it
+    // which takes O(1)
     else if (deq.back() == brackets[str[i]])
       deq.pop_back();
   }
-
+  // Return whether stack is empty or not
   return deq.empty();
 }
 
+// Now we can check BALANCE in any string!
+// Even if it has space, we can check. (as we can see in my infile.txt)
 int main() {
   ifstream inFile("infile.txt");
   ofstream outFile("outfile.txt");
--- Stack, Queue/practice4.py
+++ Stack, Queue/practice4.py
@@ -1,21 +1,39 @@
 # Practice 4. Palindromes and Balance
 from collections import deque
 
+# Check whether string is paleindrom or not
+# Return True or False
+# Time complexity : O(n),
+# Since it takes length n string, and take floor(n/2) calculation.
 def isPalindrome(string: str) -> bool:
+  # String to deque
   string = deque(list(string))
-
+  # Since I used deque, we can check in both first / last one
   while len(string) >= 2:
     if string.pop() != string.popleft():
       return False
   return True
 
+# Check whether brackets are balanced or not.
+# Return True or False
+# Time complexity : O(n)
+# Check the whole length n string, it takes O(n)
+# If it is bracket, check its matching
+# If not, append it : O(1)
+# else, pop it : O(1)
+# We can simplify it to O(n).
 def balance(string: str) -> bool:
   stack = deque()
+  # Dictionary for each bracket's pair
   brackets = {"}" : "{", "]" : "[", ")" : "("}
   for i in string:
-    if i not in "{}[]()": pass
+    # If it is not a bracket
+    if i not in "{}[]()":
+      pass
+    # stack is empty, or opening bracket : append it
     elif not stack or i in "{[(":
       stack.append(i)
+    # If it is matched well, instead of appending, pop
     elif stack[-1] == brackets[i]:
       stack.pop()
 
@@ -25,6 +43,8 @@ def balance(string: str) -> bool:
     return True
 
 if __name__ == "__main__":
+  # Instead of using "python practice4.py input.txt output.txt", just use it
+  # for vscode code runner...
   inputfile = "infile.txt"
   outputfile = "outfile.txt"
   with open(inputfile, 'r') as inFile:
@@ -41,6 +61,8 @@ if __name__ == "__main__":
         ret = "T" if isPalindrome(words[1]) else "F"
         outFile.write(ret+"\n")
 
+      # Also check even if it has space (So sentence also okay!)
+      # As we can see in infile.txt
       elif op == 'B':
         ret = "T" if balance("".join(words[1:])) else "F"
         outFile.write(ret+"\n")
